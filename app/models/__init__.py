08e57ae245c1f1a0140a09662d6fbfcd8fc1b66e
Edmund Ju <edmundju@berkeley.edu>
2022-12-19 19:52:54 -0800

    create migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db
 from .user import User
+from .article import Article
+from .coin import Coin
+from .portfolio import Portfolio
+from .transaction import Transaction
+from .watchlist_coin import WatchlistCoin
+from .watchlist import Watchlist
 from .db import environment, SCHEMA
