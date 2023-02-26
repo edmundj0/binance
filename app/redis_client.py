96210793d8c0647d96e6f46a50d1535eeea060f9
Edmund Ju <edmundju@berkeley.edu>
2023-02-26 12:57:37 -0800

    dockerize kafka & redis

--- app/redis_client.py
+++ app/redis_client.py
@@ -3,16 +3,20 @@ import redis
 import websocket
 import json
 import threading
+# from kafka.consumer import KafkaConsumer
+# from kafka.producer import KafkaProducer
 from kafka import KafkaProducer, KafkaConsumer
+from collections import deque
 
 
 #Connect to Redis
-redis_client = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, db=0)
+redis_client = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379)
 
 try:
     response = redis_client.ping()
 except redis.exceptions.ConnectionError as e:
     print(f'Unable to connect to Redis: {e}')
+    exit(1)
 
 
 #Kafka producer
@@ -22,22 +26,34 @@ producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
 consumer = KafkaConsumer(
     os.getenv('KAFKA_TOPIC'),
     bootstrap_servers=['kafka:9092'],
-    auto_offset_reset='latest',
+    auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id="my-group"
+    # group_id="my-group"
 )
 
+#push ws messages into kafka
 def on_message(ws, message):
     message = json.loads(message)
-    print(message)
-    symbol = message['s']
-    price = message['c']
-    timestamp = message['E']
+    # print(message)
+
+    producer.send(os.getenv('KAFKA_TOPIC'), json.dumps(message).encode())
+    producer.flush()
+
+    #kafka queue check
+    # if len(producer) > 10:
+
+    oldest_msg = next(consumer)
+    data = json.loads(oldest_msg.value)
+    symbol = data['s']
+    price = data['c']
+    timestamp = data['E']
 
     key = f"{symbol}_{timestamp}"
-    redis_client.set(key, price)
+    redis_client.set(key,price)
 
-    redis_client.expire(key, 5)
+    redis_client.expire(key, 30)
+
+
 
 def on_close(ws):
     print('Websocket closed')
@@ -45,12 +61,13 @@ def on_close(ws):
 def on_open(ws):
     print('Websocket opened')
 
-websocket_url = "wss://stream.binance.us:9443/ws/"
-coin_symbol = "BTC"
 
 
 
-def start_websocket():
+def start():
+
+    websocket_url = "wss://stream.binance.us:9443/ws/"
+    coin_symbol = "BTC"
 
     ws = websocket.WebSocketApp(
         websocket_url + f"{coin_symbol.lower()}usd@ticker",
@@ -62,5 +79,33 @@ def start_websocket():
     ws.run_forever()
 
 
-ws_thread = threading.Thread(target=start_websocket)
-ws_thread.start()
+if __name__ == '__main__':
+    start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def on_message(ws, message):
+#     message = json.loads(message)
+#     print(message)
+#     symbol = message['s']
+#     price = message['c']
+#     timestamp = message['E']
+
+#     key = f"{symbol}_{timestamp}"
+#     redis_client.set(key, price)
+
+#     redis_client.expire(key, 5)
