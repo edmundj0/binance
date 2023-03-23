cb6fb1dda3aba1533d8a53b2bb206abf38f46bc1
Edmund Ju <edmundju@berkeley.edu>
2023-03-22 23:04:33 -0700

    add ability to change candlestick time intervals

--- react-app/src/components/CoinDetails/CoinChartTimeIntervals.js
+++ react-app/src/components/CoinDetails/CoinChartTimeIntervals.js
@@ -1,7 +1,17 @@
-export default function CoinChartTimeIntervals() {
+export default function CoinChartTimeIntervals({ setTimeInSecs, chartContainerRef, setTimeInterval }) {
+
+    const timeOptions = [["60", "1m"], ["300", "5m"], ["900", "15m"], ["1800", "30m"], ["3600", "1h"], ["86400", "1d"], ["259200", "3d"]]
+
     return (
         <div>
-            Testttt
+            {timeOptions.map(time => {
+                return (
+                    <div key={`time ${time[0]}`} onClick={() => {
+                        setTimeInSecs(time[0])
+                        setTimeInterval(time[1])
+                    }}>{time[1]}</div>
+                )
+            })}
         </div>
     )
 }
