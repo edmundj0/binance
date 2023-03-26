c0355d440c5d995983adaa232f52ea9425ace161
Edmund Ju <edmundju@berkeley.edu>
2023-03-26 00:08:08 -0700

    remove hardcoded btc & eth routes

--- app/api/market_data_routes.py
+++ app/api/market_data_routes.py
@@ -1,36 +1,54 @@
 import requests
 from flask import Blueprint
+from app.models import Coin
 
 
 market_data_routes = Blueprint('market_data', __name__)
 
-@market_data_routes.route('/options/btc')
-def options_market_data_btc():
+@market_data_routes.route('/options/<int:coin_id>')
+def options_market_data(coin_id):
     """
-    Return options data from Ledger X API for btc options data
+    Return options data from Ledger X API for options data
     """
+    coin = Coin.query.filter(Coin.id == coin_id).first()
+    print(coin, 'coin')
+
+    if not coin:
+        return {'errors': "Coin not found or is not available at this time"}
 
     headers = {'Accept': 'application/json'}
-    response = requests.get('https://api.ledgerx.com/trading/contracts?asset=BTC', headers=headers)
-    print(response.json())
+    response = requests.get(f'https://api.ledgerx.com/trading/contracts?asset={coin.symbol}')
     if response.ok:
         data = response.json()
         return data
     else:
-        return {"error": "Unable to retrieve options data"}
+        return {'errors': f"Unable to retrieve options data"}
+
+# @market_data_routes.route('/options/btc')
+# def options_market_data_btc():
+#     """
+#     Return options data from Ledger X API for btc options data
+#     """
+
+#     headers = {'Accept': 'application/json'}
+#     response = requests.get('https://api.ledgerx.com/trading/contracts?asset=BTC', headers=headers)
+#     if response.ok:
+#         data = response.json()
+#         return data
+#     else:
+#         return {"error": "Unable to retrieve options data"}
 
 
-@market_data_routes.route('/options/eth')
-def options_market_data_eth():
-    """
-    Return options data from Ledger X API for eth options data
-    """
+# @market_data_routes.route('/options/eth')
+# def options_market_data_eth():
+#     """
+#     Return options data from Ledger X API for eth options data
+#     """
 
-    headers = {'Accept': 'application/json'}
-    response = requests.get('https://api.ledgerx.com/trading/contracts?asset=ETH', headers=headers)
-    print(response.json())
-    if response.ok:
-        data = response.json()
-        return data
-    else:
-        return {"error": "Unable to retrieve options data"}
+#     headers = {'Accept': 'application/json'}
+#     response = requests.get('https://api.ledgerx.com/trading/contracts?asset=ETH', headers=headers)
+#     if response.ok:
+#         data = response.json()
+#         return data
+#     else:
+#         return {"error": "Unable to retrieve options data"}
